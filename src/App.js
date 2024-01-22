a44082377bcfc6063ab0502932a996561d9c0cf5
saisharan6669 <124062481+saisharan6669@users.noreply.github.com>
2024-01-22 18:50:45 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
-import { useState } from "react";
-import "./App.css";
-import Header from "./components/Header";
-import TaskList from "./components/TaskList";
-import NewTask from "./components/NewTask";
+
+import './App.css';
+import Loginpage from './sai';
+
 function App() {
-  const tasks = [
-    { name: "Road Trip", date: new Date(12, 2, 2020), id: "192022" },
-    { name: "Write Book", date: new Date(12, 2, 2020), id: "2930223" },
-    { name: "School Work", date: new Date(12, 2, 2020), id: "238283" },
-    { name: "Home Work", date: new Date(12, 2, 2020), id: "2803283" },
-  ];
-  
-  const [allTasks, setAllTasks] = useState(tasks);
-  const removeId = (id) => {
-    const newList = allTasks.filter((item) => item.id !== id);
-    setAllTasks(newList);
-  };
-  const addTaskHandler=(newTaskData)=>{
-    setAllTasks([newTaskData,...allTasks])
-
-  }
-
   return (
-    <div className="App">
-      <Header />
-      <h1> Hello This is test</h1>
-      <NewTask onAddTask={addTaskHandler}/>
-      <TaskList tasks={allTasks} onRemoveHandler={removeId} />
-    </div>
+
+    <Loginpage/>
   );
 }
-
 export default App;
